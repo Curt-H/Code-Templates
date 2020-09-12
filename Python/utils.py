167b633dd89c6cf12684ba919e6507207205fede
Curt H <guardian.curt@outlook.com>
2020-09-13 01:04:58 +0800

    Update: change log format

--- Python/utils.py
+++ Python/utils.py
@@ -7,18 +7,22 @@ def log(*args, **kwargs):
 
     print(f'[{st}]')
     for i in args:
-        print(*args)
+        print(f'[{i}]')
     for k in kwargs.keys():
-        print(f'{k}={kwargs[k]}')
+        print(f'[{k}={kwargs[k]}]')
+    
+    print()
 
     with open('log.txt', 'a+', encoding='utf-8') as f:
-        print(f'[{st}]', file=f)
-        print(*args, file=f)
+        for i in args:
+            print(f'[{i}]', file=f)
         for k in kwargs.keys():
             print(f'{k}={kwargs[k]}', file=f)
+        print(file=f)
 
 
 if __name__ == '__main__':
     for i in range(10):
         log('test')
         log('a', c='fuck')
+        log('a', 'c' ,'b', abc=5)
